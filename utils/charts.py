798f43431e57899e8b4900914798560444ec34b0
piyush kumar pal <piyushkumarpal9549@gmail.com>
2025-09-08 00:47:58 +0530

    Update charts.py

--- utils/charts.py
+++ utils/charts.py
@@ -106,8 +106,8 @@ def create_bar_chart(feedback):
         margin=dict(t=40, b=0, l=0, r=0),
         annotations=[
             # Annotations for Chart 1 (top)
-            dict(text=f'{correct_perc}%', x=0.5, y=0.89, font=dict(size=30, color='#f1f5f9'), showarrow=False),
-            dict(text='Correct', x=0.5, y=0.78, font=dict(size=14, color='#94a3b8'), showarrow=False),
+            dict(text=f'{correct_perc}%', x=0.5, y=0.84, font=dict(size=30, color='#f1f5f9'), showarrow=False),
+            dict(text='Correct', x=0.5, y=0.74, font=dict(size=14, color='#94a3b8'), showarrow=False),
             # Annotations for Chart 2 (bottom)
             dict(text=f'{incorrect_perc}%', x=0.5, y=0.18, font=dict(size=30, color='#f1f5f9'), showarrow=False),
             dict(text='Incorrect', x=0.5, y=0.32, font=dict(size=14, color='#94a3b8'), showarrow=False),
