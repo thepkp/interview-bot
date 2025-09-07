97ab300263ccfc9605806ed75394db7d60aa5374
piyush kumar pal <piyushkumarpal9549@gmail.com>
2025-09-08 00:42:31 +0530

    Update charts.py

--- utils/charts.py
+++ utils/charts.py
@@ -112,7 +112,7 @@ def create_bar_chart(feedback):
             dict(text=f'{incorrect_perc}%', x=0.5, y=0.18, font=dict(size=30, color='#f1f5f9'), showarrow=False),
             dict(text='Incorrect', x=0.5, y=0.32, font=dict(size=14, color='#94a3b8'), showarrow=False),
             # Main Title
-            dict(text='Question Breakdown', x=0.5, y=1, font_size=20, font_color="#f1f5f9", showarrow=False)
+            dict(text='Question_Breakdown', x=0.5, y=1, font_size=20, font_color="#f1f5f9", showarrow=False)
         ]
     )
     return fig
